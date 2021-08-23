d6d3d72606d205115223b8e21f1cd6b04a2d7b14
ModerWarfer <86485677+ModerWarfer@users.noreply.github.com>
2021-08-23 20:29:59 -0300

    Update nsfwmenu.js

--- src/nsfwmenu.js
+++ src/nsfwmenu.js
@@ -15,9 +15,7 @@ const nsfwmenu = (prefix, pushname) => {
 
 _El bot nesecita admin y tener activado los NSFW_\n _Digita_\n ${prefix}*nsfw 1*
 
-Algunas funciones fueron eliminas por errores en el servidor de la India
-
-By Felixcrack`
+`
 
 }
 
